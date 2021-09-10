5333ff2383273a1a8d65caa934a58da504e39f69
kpkeerthi <43111336+kpkeerthi25@users.noreply.github.com>
2021-09-10 11:52:25 +0530

    initial

--- App.js
+++ App.js
@@ -2,10 +2,49 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const { Gateway,Wallets} = require('fabric-network');
+// const FabricCAServices = require('fabric-ca-client');
+
+// const walletPath = './assets/wallet'
+// let ccp = require('./assets/connection-org1.json')
+
+
+
+
+const getAllAssets = async () => {
+
+//Returns a "hello world" message to show interaction with the native module
+  // await HlfSdk.connectionProfileSetup(ccp);
+  const org1UserId = 'appUser';
+  const channelName = "mychannel";
+  const chaincodeName = "basic";
+
+  const wallet = await Wallets.newFileSystemWallet(walletPath);
+
+
+  const gateway = new Gateway();
+            await gateway.connect(ccp, {
+                wallet,
+                identity:org1UserId,
+                discovery: { enabled: true, asLocalhost: true },
+            });
+
+  // Build a network instance based on the channel where the smart contract is deployed
+  const network = await gateway.getNetwork(channelName);
+
+  // Get the contract from the network.
+  const contract = network.getContract(chaincodeName);
+
+  let result = await contract.evaluateTransaction('GetAllAssets');
+
+  return JSON.parse(result);
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{{result}}</Text>
       <StatusBar style="auto" />
     </View>
   );
